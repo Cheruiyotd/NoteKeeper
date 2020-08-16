0f0ac45e995ea39f2304a642da64866a076b7a13
Cheruiyot <korirkipnisden@gmail.com>
2020-08-17 00:48:51 +0300

    Course: Working with Android Tools and Testing module 3 used createNewNoteOneSteCreation() for test driven development

--- app/src/test/java/com/mcheru/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/mcheru/notekeeper/DataManagerTest.java
@@ -19,18 +19,17 @@ public class DataManagerTest {
     }
     @Test
     public void createNewNote() {
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText = "This is the body text of my test note.";
 
-        int noteIndex = dm.createNewNote();
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        int noteIndex = sDataManager.createNewNote();
+        NoteInfo newNote = sDataManager.getNotes().get(noteIndex);
         newNote.setCourse(course);
         newNote.setTitle(noteTitle);
         newNote.setText(noteText);
 
-        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
         assertEquals(course, compareNote.getCourse());
         assertEquals(noteTitle, compareNote.getTitle());
         assertEquals(noteText, compareNote.getText());
@@ -59,5 +58,17 @@ public class DataManagerTest {
 
         int foundIndex2 = sDataManager.findNote(newNote2);
         assertEquals(noteIndex2, foundIndex2);
+    }
+    @Test
+    public void createNewNoteOneSteCreation(){
+        final CourseInfo course = sDataManager.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body text of my test note.";
+        int noteIndex = sDataManager.createNewNote(course, noteTitle, noteText);
+
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
+        assertEquals(course, compareNote.getCourse());
+        assertEquals(noteTitle, compareNote.getTitle());
+        assertEquals(noteText, compareNote.getText());
     }
 }
