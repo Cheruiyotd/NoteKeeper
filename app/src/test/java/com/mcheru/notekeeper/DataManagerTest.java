21011fd6fc1d0e572242a0e81afd352769a2d537
Cheruiyot <korirkipnisden@gmail.com>
2020-08-17 00:32:31 +0300

    Course: Working with Android Tools and Testing module 3 added @BeforeClass test preprocessing

--- app/src/test/java/com/mcheru/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/mcheru/notekeeper/DataManagerTest.java
@@ -1,16 +1,21 @@
 package com.mcheru.notekeeper;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DataManagerTest {
+    static DataManager sDataManager;
+    @BeforeClass
+    public static void classSetUp(){
+        sDataManager = DataManager.getInstance();
+    }
     @Before
     public void setUp(){
-        DataManager dm = DataManager.getInstance();
-        dm.getNotes().clear();
-        dm.initializeExampleNotes();
+        sDataManager.getNotes().clear();
+        sDataManager.initializeExampleNotes();
     }
     @Test
     public void createNewNote() {
@@ -32,28 +37,27 @@ public class DataManagerTest {
     }
     @Test
     public void findSemilarNotes(){
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText = "This is the body text of my test note.";
         final String noteText2 = "This is the body text of my second test note.";
 
-        int noteIndex1 = dm.createNewNote();
-        NoteInfo newNote1 = dm.getNotes().get(noteIndex1);
+        int noteIndex1 = sDataManager.createNewNote();
+        NoteInfo newNote1 = sDataManager.getNotes().get(noteIndex1);
         newNote1.setCourse(course);
         newNote1.setTitle(noteTitle);
         newNote1.setText(noteText);
 
-        int noteIndex2 = dm.createNewNote();
-        NoteInfo newNote2 = dm.getNotes().get(noteIndex2);
+        int noteIndex2 = sDataManager.createNewNote();
+        NoteInfo newNote2 = sDataManager.getNotes().get(noteIndex2);
         newNote2.setCourse(course);
         newNote2.setTitle(noteTitle);
         newNote2.setText(noteText2);
 
-        int foundIndex1 = dm.findNote(newNote1);
+        int foundIndex1 = sDataManager.findNote(newNote1);
         assertEquals(noteIndex1, foundIndex1);
 
-        int foundIndex2 = dm.findNote(newNote2);
+        int foundIndex2 = sDataManager.findNote(newNote2);
         assertEquals(noteIndex2, foundIndex2);
     }
 }
