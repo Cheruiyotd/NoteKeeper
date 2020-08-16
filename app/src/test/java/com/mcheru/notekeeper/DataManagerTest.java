52f0dfce18b01d0fdc6fdc6315100ddbbf8c103a
Cheruiyot <korirkipnisden@gmail.com>
2020-08-16 23:49:03 +0300

    Course: Working with Android Tools and Testing module 3 Added jUnit4 jvm tests

---
+++ app/src/test/java/com/mcheru/notekeeper/DataManagerTest.java
@@ -0,0 +1,27 @@
+package com.mcheru.notekeeper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataManagerTest {
+
+    @Test
+    public void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        final CourseInfo course = dm.getCourse("android_async");
+        final String noteTitle = "Text note title";
+        final String noteText = "This is the body text of my note test.";
+
+        int noteIndex = dm.createNewNote();
+        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        newNote.setCourse(course);
+        newNote.setTitle(noteTitle);
+        newNote.setText(noteText);
+
+        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        assertEquals(compareNote.getCourse(), course);
+        assertEquals(compareNote.getTitle(), noteTitle);
+        assertEquals(compareNote.getText(), noteText);
+    }
+}
