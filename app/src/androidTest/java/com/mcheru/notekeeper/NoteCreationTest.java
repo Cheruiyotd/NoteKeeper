c88da57b5c74af290f79ca1780ab87d3dad69a29
Cheruiyot <korirkipnisden@gmail.com>
2020-08-17 04:07:16 +0300

    Course: Working with Android Tools and Testing module 4 Implemented spinner and back button interaction instrument test

--- app/src/androidTest/java/com/mcheru/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/mcheru/notekeeper/NoteCreationTest.java
@@ -1,10 +1,12 @@
 package com.mcheru.notekeeper;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,18 +15,32 @@ import static org.junit.Assert.*;
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+    static DataManager sDataManager;
+    @BeforeClass
+    public static void classSetUp(){
+        sDataManager = DataManager.getInstance();
+    }
     @Rule
     public ActivityTestRule<NoteListActivity> mNoteListActivityRule =
             new ActivityTestRule<>(NoteListActivity.class);
     @Test
     public void createNewNote(){
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+        final  String noteTitle = "Auto type test title";
+        final  String noteText = "Auto type test text";
         //ViewInteraction viewInteraction = onView(withId(R.id.floatingActionButton)).perform(click());
         onView(withId(R.id.floatingActionButton)).perform(click());
-        onView(withId(R.id.text_note_title)).perform(typeText("Auto type test title"));
-        onView(withId(R.id.text_note_text)).perform(typeText("Auto type test body"),
+        onView(withId(R.id.spinner_courses)).perform(click());
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText),
                 ViewActions.closeSoftKeyboard());
+
+        Espresso.pressBack();
     }
 }
